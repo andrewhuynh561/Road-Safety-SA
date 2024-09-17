cf537532c4c796e93ea833f5799dbcb52ad0e438
Chanh Hai Huynh <huyhc001@mymail.unisa.edu.au>
2024-09-17 22:25:34 +0930

    Updated view Index with viewModel

---
+++ Assig1/ViewModels/OffenceSearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Assig1.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Assig1.ViewModels
+
+{
+    public class OffenceSearchViewModel
+    {
+        public string SearchText {  get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public SelectList CategoryList { get; set; }
+
+        public List<Offence> Offences { get; set; }
+    }
+}
