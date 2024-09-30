ea3de43372a5e9408604798a7b5be9adea1c5322
Chanh Hai Huynh <huyhc001@mymail.unisa.edu.au>
2024-09-30 17:28:57 +0930

    created ExpiationDetailViewModel

---
+++ Assig1/ViewModels/ExpiationDetailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assig1.ViewModels
+{
+    public class ExpiationDetailViewModel
+    {
+        [Display(Name = "Incident Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+
+        public DateOnly IncidentStartDate { get; set; }
+
+        [Display(Name = "Issue Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateOnly? IssueDate { get; set; }
+
+        [Display(Name = "Total Fee")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public int? TotalFee { get; set; }
+
+        [Display(Name = "Status")]
+        public string Status { get; set; }
+    }
+}
