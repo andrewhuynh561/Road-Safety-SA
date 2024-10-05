1d0f91977cfe70b2db9d0240af03538e8b9741e0
Chanh Hai Huynh <huyhc001@mymail.unisa.edu.au>
2024-10-05 21:10:27 +0930

    Created a expiatation insight view model

---
+++ Assig1/ViewModels/ExpiationInsightsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assig1.ViewModels
+{
+    public class ExpiationInsightsViewModel
+    {
+        [Display(Name="Maximum Fine")]
+        [DisplayFormat(DataFormatString ="{0:C}")]
+        public int MaxFine {  get; set; }
+
+
+        [Display(Name = "Min Fine")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public int MinFine { get; set; }
+
+        public double AvgFine { get; set; }
+
+        public int MaxSpeed { get; set; }
+
+        public int MinSpeed { get; set; }
+
+
+        [Display(Name = "Frequency")]
+        public int Frequency { get; set; }
+    }
+}
